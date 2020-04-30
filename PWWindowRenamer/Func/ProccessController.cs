f208d7c2bac5701a04fc6188bd033b1e9723d266
ufa3110 <58032853+ufa3110@users.noreply.github.com>
2020-05-01 04:31:18 +0500

    v0
    init push

---
+++ PWWindowRenamer/Func/ProccessController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace PWWindowRenamer.Func
+{
+    public class ProccessController
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        public Task Start(string cilentFullPath, String WindowTitle)
+        {
+            var processInfo = new ProcessStartInfo(cilentFullPath);
+            processInfo.WorkingDirectory = cilentFullPath.Substring(0, cilentFullPath.LastIndexOf("//") + 2);
+            var startedProcess = Process.Start(processInfo);
+            while (startedProcess.MainWindowHandle ==  (IntPtr) 0)
+            {
+                Task.Delay(1000);
+            }
+            SetWindowText(startedProcess.MainWindowHandle, WindowTitle);
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ PWWindowRenamer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PWWindowRenamer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var clientPath = (args.Length > 0) ? args[0] : "";
+            var title = (args.Length > 1) ? args[1] : "";
+            var processService = new Func.ProccessController();
+            processService.Start(clientPath, title);
+        }
+    }
+}
